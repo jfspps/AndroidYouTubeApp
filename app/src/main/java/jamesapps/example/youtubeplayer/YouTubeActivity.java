ad15e9ce442c0f09ebc91519f58816e2d24865ad
jfspps <jfsapps@gmail.com>
2020-12-14 16:11:16 +0000

    first commit, setup YouTubePlayer lib and view

---
+++ app/src/main/java/jamesapps/example/youtubeplayer/YouTubeActivity.java
@@ -0,0 +1,44 @@
+package jamesapps.example.youtubeplayer;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class YouTubeActivity extends YouTubeBaseActivity
+        implements YouTubePlayer.OnInitializedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_you_tube);
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.activity_youtube);
+
+        // building an activity by code (as opposed to using the designer)
+//        Button button1 = new Button(this);
+//        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300, 80));
+//        button1.setText("Button added");
+//        constraintLayout.addView(button1);
+
+        // again, built using code...
+        YouTubePlayerView playerView = new YouTubePlayerView(this);
+        playerView.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+        constraintLayout.addView(playerView);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
+}
